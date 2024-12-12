28a0a88d801566a7507ff007539d5fd11bd00c15
nhenan <henan.noel@ynov.com>
2024-12-12 12:02:31 +0100

    Add the path to login in web.php

--- Students-Services/routes/web.php
+++ Students-Services/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,5 @@ Route::get('/myRoute', function () {
 
 Route::get('/register', [RegisterController::class, 'create']);
 Route::post('/register', [RegisterController::class, 'store']);
+Route::post('/login', [LoginController::class, 'create']);
+Route::post('/login', [LoginController::class, 'store']);
